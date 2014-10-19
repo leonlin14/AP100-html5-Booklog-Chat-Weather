ec90c8c96478060ef133250effea5d6c80f88fcd
leonlin14 <leonlin14@hotmail.com>
2014-10-19 02:56:44 +0000

    check websocket connect

--- weather/javascripts/main.js
+++ weather/javascripts/main.js
@@ -27,6 +27,21 @@
             $('#weather-icon').addClass('wi-day-cloudy');
 	    },
 	    complete: function(jqXHR, textStatus) {
+            var ws = new WebSocket("ws://deposit-simple.codio.io:3000/", "echo-protocol"); 
+            ws.onopen = function(evt) {
+                $('[data-status]').addClass('hide');
+                $('[data-status="Success"]').removeClass('hide');
+            };
+            ws.onclose = function(evt) {
+                
+            };
+            ws.onerror = function(evt) {
+                $('[data-status]').addClass('hide');
+                $('[data-status="Error"]').removeClass('hide');
+            };
+            ws.onmessage = function(evt) {
+                
+            };
 	    }
 	});
 }) ($);
