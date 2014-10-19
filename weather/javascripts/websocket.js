b3ab93191ba5967afec02d4751827a099e92bc47
leonlin14 <leonlin14@hotmail.com>
2014-10-19 03:19:50 +0000

    make a jquery function

---
+++ weather/javascripts/websocket.js
@@ -0,0 +1,27 @@
+/**
+* Created with AP100-html5-Booklog.
+* User: leonlin14
+* Date: 2014-10-19
+* Time: 03:12 AM
+* To change this template use Tools | Templates.
+
+* Make a jQuery Modules
+*/
+
+$.fn.createWebSocket = function() {
+    var ws = new WebSocket("ws://deposit-simple.codio.io:3000/", "echo-protocol"); 
+    ws.onopen = function(evt) {
+        $('[data-status]').addClass('hide');
+        $('[data-status="Success"]').removeClass('hide');
+    };
+    ws.onclose = function(evt) {
+        $('[data-status]').addClass('hide');
+        $('[data-status="Close"]').removeClass('hide');
+    };
+    ws.onerror = function(evt) {
+        $('[data-status]').addClass('hide');
+        $('[data-status="Error"]').removeClass('hide');
+    };
+    ws.onmessage = function(evt) {
+    };
+};
